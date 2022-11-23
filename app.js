35603fd2532491719645e87300f19b027a213cef
Kot <kotvnn@msn.com>
2022-11-23 12:25:26 +0700

    init

---
+++ app.js
@@ -0,0 +1,58 @@
+const rl = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+let caseNum = 0;
+let count = 1;
+const maxCase = 100;
+const maxScore = 10;
+
+const guess = (el) => {
+	const x = parseInt(el.split(' ')[0]);
+	const y = parseInt(el.split(' ')[1]);
+	const regex = /^\d \d/g;
+	if (!regex.test(el)) {
+		console.error('Score must be separated by a space');
+		return rl.close();
+	}
+	if (x >= maxScore || y >= maxScore) {
+		console.error('Score need small or equal ' + maxScore);
+		return rl.close();
+	}
+	let count = 1;
+	if (x === 0 || y === 0) count = 1;
+	else {
+		for (let i = 1; i <= x; i++) {
+			for (let j = 1; j <= y; j++) {
+				count++;
+			}
+		}
+	}
+	console.info(count);
+}
+
+const inputScore = () => {
+	if (count <= caseNum) {
+		rl.question(`Case ${count}, score is: `, function (score) {
+			guess(score);
+			count++;
+			inputScore();
+		});
+	}
+};
+
+rl.question('How many case do you want ? ', function (n) {
+	const number = parseInt(n);
+	if (number <= maxCase) {
+		caseNum = parseInt(n);
+		inputScore();
+	} else {
+		console.error('Case need small or equal ' + maxCase);
+		rl.close();
+	}
+});
+
+rl.on('close', function () {
+	console.log('\nBYE BYE !!!');
+	process.exit(0);
+});
